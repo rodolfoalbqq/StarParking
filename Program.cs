e6b24d0ff7c49f64991ad216336ae2bf9ddd2aa1
Rodolfo Albuquerque <rodolfo.albqq@gmail.com>
2023-11-22 16:53:23 -0300

    add carros e lista de veiculos ok

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using StarParking;
+using StarParking.models;
+
+
+decimal valorFixo = 0;
+decimal valorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao Star Parking!");
+
+
+Estacionamento es = new Estacionamento(valorFixo, valorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
---
+++ models/StarParking.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarParking.models
+{
+    public class Estacionamento(decimal valorFixo, decimal valorHora)
+    {
+        decimal valorFixo = 0;
+        decimal valorHora = 0;
+
+        private List<string> veiculos = new List<string>();
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo");
+            veiculos.Add(Console.ReadLine());
+            Console.WriteLine("Veiculo adicionado");
+
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo para remover.");
+
+
+        }
+
+        public void ListarVeiculos()
+        {
+            foreach(string i in veiculos){
+
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+
+    
+}
